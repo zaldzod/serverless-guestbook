db8c7247100902cfbf5f4c01e33f44a6bf30aea1
zaldzod <chizalumecheta@hotmail.com>
2022-06-24 19:16:56 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = https://3bdb0933.us-south.apigw.appdomain.cloud/guestbook;
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
